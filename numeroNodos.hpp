ffb75d9d74c15affadb5f2c80182aa37bd98fac6
ProgrammingKiddo <programming.edu@protonmail.com>
2018-04-03 20:19:30 +0200

    Arreglado un commit anterior donde se eliminó un parámetro necesario de la función cuentaNodos(). Añadida una función llamadora general de la misma (para el cálculo de árboles enteros).

--- numeroNodos.hpp
+++ numeroNodos.hpp
@@ -2,17 +2,24 @@
 #define NUMERO_NODOS_H
 
 
-//Postcondición: devuelve el número de nodos del árbol.
+//Función base, llama a la función genérica con el nodo raíz como segundo parámetro
 template <typename T>
     int cuentaNodos(Abin<T>& arbol)
     {
-        if(n == Abin<T>::NODO_NULO)
+        return cuentaNodos(arbol, arbol.raizB());
+    }
+
+//Postcondición: devuelve el número de nodos del árbol.
+template <typename T>
+    int cuentaNodos(Abin<T>& arbol, typename Abin<T>::nodo nodo)
+    {
+        if(nodo == Abin<T>::NODO_NULO)
         {
             return 0;
         }
         else
         {
-            return 1 + cuentaNodos(arbol, arbol.hijoIzqdoB(n)) + cuentaNodos(arbol, arbol.hijoDrchoB(n));
+            return 1 + cuentaNodos(arbol, arbol.hijoIzqdoB(n)) + cuentaNodos(arbol, arbol.hijoDrchoB(nodo));
         }
     }
 
