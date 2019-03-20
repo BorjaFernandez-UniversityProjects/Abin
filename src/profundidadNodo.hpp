ac808540116aee374afb53b984a044d3281c0dac
ProgrammingKiddo <programming.edu@protonmail.com>
2019-03-20 22:08:13 +0100

    Corregido un typo al usar la variable Arbol.

--- src/profundidadNodo.hpp
+++ src/profundidadNodo.hpp
@@ -11,7 +11,7 @@ template <typename T>
         }
         else
         {
-            if(nodo == arbol.raizB())
+            if(nodo == Arbol.raizB())
             {
                 return 0;
             }
